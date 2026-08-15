d47207b6ba54038dac58e5b47812f738c577f29e
aaqib khan <sadikkhan986@gmail.com>
2026-08-15 15:17:12 +0530

    execeptionhandeling

---
+++ Exception_handling.java
@@ -0,0 +1,28 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Exception_handling {
+    public static void main(String[] args) {
+        try {
+            int n1, n2;
+            int result;
+            Scanner sc = new Scanner(System.in);
+            System.out.println("enter n1");
+            System.out.println("enter n2");
+            n1 = sc.nextInt();
+
+            System.out.println("enter n2");
+            n2 = sc.nextInt();
+
+            result = n1 / n2;
+            System.out.println("result :" + result);
+
+        } catch (InputMismatchException e) {
+            System.out.println("enter numerical values");
+        } catch (ArithmeticException ae) {
+
+        } catch (Exception e) {
+            System.out.println("invalid input");
+        }
+    }
+}
